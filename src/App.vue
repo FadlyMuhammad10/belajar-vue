fbb9b99d425b54768a8de26688a5dbf06390f067
Muhammad Fadly <davit9747@gmail.com>
2025-05-13 14:55:36 +0700

    basic component

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainButton from './components/MainButton.vue'
+const name = 'John Doe'
+</script>
+
+<template>
+  <MainButton></MainButton>
+  <h1>{{ name }}</h1>
+</template>
+
+<style scoped></style>
