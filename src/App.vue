1180e967ab03e71a96993eb8e9f6c9a6ae5f5dad
Muhammad Fadly <davit9747@gmail.com>
2025-05-14 10:35:04 +0700

    Reactivity State

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { reactive, ref } from 'vue'
 import MainButton from './components/MainButton.vue'
 // import OptionsComponent from './components/OptionsComponent.vue'
 // import CompositionComponent from './components/CompositionComponent.vue'
 const name = 'John Doe'
 const body = '<h1 style="color: red">ini description</h1>'
 // const idData = 'data-1'
+
+const count = ref(0)
+
+const user = reactive({
+  name: 'John Doe',
+  age: 30,
+})
 </script>
 
 <template>
@@ -19,6 +27,13 @@ const body = '<h1 style="color: red">ini description</h1>'
 
   <!-- ini menggunakan v-html/raw  -->
   <div :id="idData" class="description" v-html="body"></div>
+
+  <button @click="count++">count is: {{ count }}</button>
+
+  <div class="">
+    <p>name: {{ user.name }}</p>
+    <p>age: {{ user.age }}</p>
+  </div>
 </template>
 
 <style scoped></style>
