2b19a3a4a6acc91dc33366577bfee2423c3eac84
Muhammad Fadly <davit9747@gmail.com>
2025-05-14 11:35:02 +0700

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,28 @@
 <script setup>
-import { reactive, ref } from 'vue'
-import MainButton from './components/MainButton.vue'
-// import OptionsComponent from './components/OptionsComponent.vue'
-// import CompositionComponent from './components/CompositionComponent.vue'
-const name = 'John Doe'
-const body = '<h1 style="color: red">ini description</h1>'
-// const idData = 'data-1'
+import { computed, reactive, ref } from 'vue'
+
+const author = reactive({
+  name: 'Jk Rowling',
+  books: ['Harry Potter', 'Sorcerer Stone', 'Chamber of Secrets'],
+})
 
 const count = ref(0)
 
-const user = reactive({
-  name: 'John Doe',
-  age: 30,
+const increment = () => {
+  count.value++
+}
+
+const publishBook = computed(() => {
+  console.log('test')
+  return author.books.length > 0 ? 'Yes' : 'No'
 })
 </script>
 
 <template>
-  <MainButton :name="'Button 1'"></MainButton>
-  <MainButton name="Button 2" disabled="true"></MainButton>
+  <div class="container">
+    <p>{{ publishBook }}</p>
 
-  <!-- ini menggunakan interpolation -->
-  <h1>{{ name }}</h1>
-
-  <OptionsComponent />
-  <CompositionComponent />
-
-  <!-- ini menggunakan v-html/raw  -->
-  <div :id="idData" class="description" v-html="body"></div>
-
-  <button @click="count++">count is: {{ count }}</button>
-
-  <div class="">
-    <p>name: {{ user.name }}</p>
-    <p>age: {{ user.age }}</p>
+    <button @click="increment">{{ count }}</button>
   </div>
 </template>
 
